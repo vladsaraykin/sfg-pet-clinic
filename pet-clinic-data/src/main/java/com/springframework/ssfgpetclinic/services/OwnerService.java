c5c6482ecbea03e20f72c8ce81ac07978e12fb56
v.saraikin <v.saraikin@infomaximum.com>
2020-12-02 18:30:22 +0300

    refactor services. closes #11

--- pet-clinic-data/src/main/java/com/springframework/ssfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/springframework/ssfgpetclinic/services/OwnerService.java
@@ -2,15 +2,7 @@ package com.springframework.ssfgpetclinic.services;
 
 import com.springframework.ssfgpetclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
-
-    Owner findById(Long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
 }
