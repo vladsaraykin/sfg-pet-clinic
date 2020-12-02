370388be48cd18e84fd4e915dfe573ebe63b2ad7
v.saraikin <v.saraikin@infomaximum.com>
2020-12-02 18:49:44 +0300

    Impl Map based Service. closes #6

---
+++ pet-clinic-data/src/main/java/com/springframework/ssfgpetclinic/services/map/VetServiceMap.java
@@ -0,0 +1,33 @@
+package com.springframework.ssfgpetclinic.services.map;
+
+import com.springframework.ssfgpetclinic.model.Vet;
+import com.springframework.ssfgpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+}
