2c965a13dae7ee6a30a4fee2e0a765fd6b82ec12
v.saraikin <v.saraikin@infomaximum.com>
2020-12-04 11:48:01 +0300

    added PetTypeService. closes #29

---
+++ pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package com.springframework.sfgpetclinic.services;
+
+import com.springframework.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
