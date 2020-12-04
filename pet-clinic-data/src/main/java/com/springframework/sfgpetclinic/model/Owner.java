1227e6bc4a7b227eeee87dd11a04c8e733917dec
v.saraikin <v.saraikin@infomaximum.com>
2020-12-04 11:28:04 +0300

    added visits, updated owner with pet reletionship. closes #26

--- pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/model/Owner.java
@@ -1,4 +1,16 @@
 package com.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person {
+
+    private Set<Pet> petSet;
+
+    public Set<Pet> getPetSet() {
+        return petSet;
+    }
+
+    public void setPetSet(Set<Pet> petSet) {
+        this.petSet = petSet;
+    }
 }
