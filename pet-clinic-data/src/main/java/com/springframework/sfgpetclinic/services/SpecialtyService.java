aa9eb10fad5f743d2654ce2e5251820456decd7c
v.saraikin <v.saraikin@infomaximum.com>
2020-12-04 12:16:19 +0300

    added Specialty service map impl. closes #31

---
+++ pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/services/SpecialtyService.java
@@ -0,0 +1,6 @@
+package com.springframework.sfgpetclinic.services;
+
+import com.springframework.sfgpetclinic.model.Specialty;
+
+public interface SpecialtyService extends CrudService<Specialty, Long> {
+}
