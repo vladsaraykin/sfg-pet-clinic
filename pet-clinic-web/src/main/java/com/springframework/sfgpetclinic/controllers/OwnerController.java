ff95b792036a0c667e573855d34a63809ec5f695
v.saraikin <v.saraikin@infomaximum.com>
2020-12-07 18:04:33 +0300

    added tests. closes #49

--- pet-clinic-web/src/main/java/com/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
