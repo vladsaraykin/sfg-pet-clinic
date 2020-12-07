c766f0a8d790a1fc09a3cb4f42d52bae1086c7b1
v.saraikin <v.saraikin@infomaximum.com>
2020-12-07 10:41:51 +0300

    Implemented Map Imp for visit service

---
+++ pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/services/map/VisitMapService.java
@@ -0,0 +1,37 @@
+package com.springframework.sfgpetclinic.services.map;
+
+import com.springframework.sfgpetclinic.model.Visit;
+import com.springframework.sfgpetclinic.services.VisitService;
+
+import java.util.Set;
+
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if (visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId() == null) {
+            throw new RuntimeException("Invalid visit");
+        }
+        return super.save(visit);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Visit visit) {
+        super.delete(visit);
+    }
+}
