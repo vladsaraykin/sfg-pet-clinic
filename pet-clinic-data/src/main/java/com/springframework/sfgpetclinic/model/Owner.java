f4dbf929f2eb2bfa9bf9e2fb7005c028e98121d3
v.saraikin <v.saraikin@infomaximum.com>
2020-12-07 16:28:23 +0300

    refactoring models

--- pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/model/Owner.java
@@ -17,8 +17,8 @@ import java.util.Set;
 public class Owner extends Person {
 
     @Builder
-    public Owner(String firstName, String lastName, String address, String city, String telephone, Set<Pet> pets) {
-        super(firstName, lastName);
+    public Owner(Long id, String firstName, String lastName, String address, String city, String telephone, Set<Pet> pets) {
+        super(id, firstName, lastName);
         this.address = address;
         this.city = city;
         this.telephone = telephone;
@@ -27,8 +27,10 @@ public class Owner extends Person {
 
     @Column(name = "address")
     private String address;
+
     @Column(name = "city")
     private String city;
+
     @Column(name = "telephone")
     private String telephone;
 
