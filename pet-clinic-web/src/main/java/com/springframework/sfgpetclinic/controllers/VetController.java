f3dad2275844ce6e7e96ec59793d43562662ea67
v.saraikin <v.saraikin@infomaximum.com>
2020-12-14 12:59:35 +0300

    added json endpoint for vet controller

--- pet-clinic-web/src/main/java/com/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/springframework/sfgpetclinic/controllers/VetController.java
@@ -1,9 +1,14 @@
 package com.springframework.sfgpetclinic.controllers;
 
+import com.springframework.sfgpetclinic.model.Vet;
 import com.springframework.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @RequestMapping({"/vets", "/vets.html"})
 @Controller
@@ -19,5 +24,11 @@ public class VetController {
     public String listVets(Model model) {
         model.addAttribute("vets", vetService.findAll());
         return "vets/vets";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetJson() {
+
+        return vetService.findAll();
     }
 }
